b1e7a354a09f5305136ae636323426b2180b28bc
Laurence <2165020@slu.edu.ph>
2018-05-01 17:53:52 +0800

    some parts developed ~

--- src/main/java/corbaclient/HangmanClient.java
+++ src/main/java/corbaclient/HangmanClient.java
@@ -8,56 +8,79 @@ import org.omg.CosNaming.NamingContextExtHelper;
 
 import javax.swing.*;
 
-public class HangmanClient {
-    static Hangman hangman;
+public class HangmanClient extends JFrame {
+    private static Hangman hangman;
+    private MainPanel mainPanel;
+    private Processor processor;
+
+    public HangmanClient() {
+        bootFrame();
+    }
 
     public static void main(String[] args) {
-        if (false) {
-            try {
-                // create and initialize the ORB
-                ORB orb = ORB.init(args, null);
 
-                // get the root naming context
-                org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-                // Use NamingContextExt instead of NamingContext. This is
-                // part of the Interoperable naming Service.
-                NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+        bootCorbaContext(args);
 
-                // resolve the Object Reference in Naming
-                String name = "HangmanGame";
-                hangman = HangmanHelper.narrow(ncRef.resolve_str(name));
-
-                System.out.println("Obtained a handle on server object: " + hangman);
-            } catch (Exception e) {
-                System.out.println("ERROR : " + e);
-                e.printStackTrace(System.out);
-            }
-        }
         //Boot The UI
+        new HangmanClient();
+    }
+
+
+    private void bootFrame() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JFrame frame = new JFrame();
 
-                frame.add(new MainPanel(new MyLetterReceiver()));
-                //f.setComponentOrientation(null);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(580, 500);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                String name = null;
-                while (name == null)
-                    name = JOptionPane.showInputDialog(frame, "Please enter your name!");
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setSize(580, 500);
+                setLocationRelativeTo(null);
+                setVisible(true);
+                String name = JOptionPane.showInputDialog("Please enter your name!");
 
-                frame.setTitle(String.format("Hi %s! Let's play Hangman", name));
+                if (name != null && !name.isEmpty()) {
+                    processor = new Processor();
+                    mainPanel = new MainPanel(processor);
+                    add(mainPanel);
+                    revalidate();
+                    repaint();
+                } else
+                    System.exit(1);
             }
         });
     }
 
-    public static class MyLetterReceiver implements LetterReceiver{
+    public static void bootCorbaContext(String[] args) {
+        try {
+            // create and initialize the ORB
+            ORB orb = ORB.init(args, null);
+
+            // get the root naming context
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            // Use NamingContextExt instead of NamingContext. This is
+            // part of the Interoperable naming Service.
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            // resolve the Object Reference in Naming
+            String name = "HangmanGame";
+            hangman = HangmanHelper.narrow(ncRef.resolve_str(name));
+
+            System.out.println("Obtained a handle on server object: " + hangman);
+        } catch (Exception e) {
+            System.out.println("ERROR : " + e);
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public class Processor implements LetterReceiver {
+
+        private String name;
 
         @Override
         public void getChar(char userChoice) {
             System.out.println(userChoice);
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 }
